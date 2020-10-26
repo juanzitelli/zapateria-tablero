28735b6e8b43ccd4eaca3cdd1be5710289a377c0
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2020-10-25 23:53:45 -0300

    More bugs... :(

--- docs/precache-manifest.c5a0442efcc87d3b64fd97b71259808b.js
+++ docs/precache-manifest.c5a0442efcc87d3b64fd97b71259808b.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "9df83e3bca27f188a807e3f4f2b641de",
-    "url": "./index.html"
+    "url": "/index.html"
   },
   {
     "revision": "cf6dad6218947abc64e5",
-    "url": "./static/css/main.58a19899.chunk.css"
+    "url": "/static/css/main.58a19899.chunk.css"
   },
   {
     "revision": "68287b42aff7b556fa70",
-    "url": "./static/js/2.c5afa41f.chunk.js"
+    "url": "/static/js/2.c5afa41f.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "./static/js/2.c5afa41f.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.c5afa41f.chunk.js.LICENSE.txt"
   },
   {
     "revision": "cf6dad6218947abc64e5",
-    "url": "./static/js/main.eaa10f42.chunk.js"
+    "url": "/static/js/main.eaa10f42.chunk.js"
   },
   {
     "revision": "1a43e52913820da9f859",
-    "url": "./static/js/runtime-main.862430ec.js"
+    "url": "/static/js/runtime-main.862430ec.js"
   }
 ]);
