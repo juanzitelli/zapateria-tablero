e675491f2329311e2a91a980b3a6ad646966807a
Juan Agustín Zitelli <juanzitelli7@gmail.com>
2020-10-27 12:56:49 -0300

    Homepage : .

---
+++ docs/precache-manifest.bd0ecc21487069fb4c82889a90847363.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930a84b21053adafbb7b72c7da09d312",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b50b066c86712eea82b",
+    "url": "./static/css/main.58a19899.chunk.css"
+  },
+  {
+    "revision": "769aa695884ed6a9b415",
+    "url": "./static/js/2.730ccaa9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.730ccaa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b50b066c86712eea82b",
+    "url": "./static/js/main.1992ce3f.chunk.js"
+  },
+  {
+    "revision": "bb9d2b397c2e6aaf4d19",
+    "url": "./static/js/runtime-main.4b1b5ef1.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ae894b8dea0280421c75696d1aced1b5.js"
+  "./precache-manifest.bd0ecc21487069fb4c82889a90847363.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
